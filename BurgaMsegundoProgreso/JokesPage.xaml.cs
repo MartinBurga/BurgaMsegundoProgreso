01350a95846cc6cb8abb2bd8d9eee8e56f4be95f
MartinBurga <mburga.xixv@gmail.com>
2025-06-02 08:33:12 -0500

    Creacion de modelo y implementacion de API de chistes.

--- BurgaMsegundoProgreso/JokesPage.xaml.cs
+++ BurgaMsegundoProgreso/JokesPage.xaml.cs
@@ -2,8 +2,35 @@ namespace BurgaMsegundoProgreso;
 
 public partial class JokesPage : ContentPage
 {
-	public JokesPage()
+	private readonly JokesService _jokesService;
+    public JokesPage()
 	{
 		InitializeComponent();
-	}
+		var httpClient = new HttpClient();
+        _jokesService = new JokesService(httpClient);
+		loadJoke();
+    }
+
+	private async Task<> loadJoke()
+    {
+        try
+        {
+            var joke = await _jokesService.GetRandomJokeAsync();
+            if (joke != null)
+            {
+                jokeLabel.Text = $"{joke.setup}\n\n{joke.punchline}";
+                se
+            }
+            else
+            {
+                jokeLabel.Text = "No joke available at the moment.";
+            }
+        }
+        catch (Exception ex)
+        {
+            jokeLabel.Text = "Error loading joke: " + ex.Message;
+        }
+    }
+
+
 }
