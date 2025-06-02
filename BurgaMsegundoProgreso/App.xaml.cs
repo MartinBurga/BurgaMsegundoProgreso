ac4ae36f28f8dd3b257ca625660b6b3d01566ebe
laboratorio <laboratorio@UPOAULA10716>
2025-06-02 07:48:45 -0500

    Agregar archivos de proyecto.

---
+++ BurgaMsegundoProgreso/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BurgaMsegundoProgreso
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BurgaMsegundoProgreso/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BurgaMsegundoProgreso
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
